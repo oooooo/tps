8e9eccd95de308e9429dab652e9fe9e752f20d59
olivia bti <olivia.at.bit@gmail.com>
2024-11-25 19:06:06 +0800

    Split files, real data, district btn add hospital numbers

---
+++ hospital-data.js
@@ -0,0 +1,690 @@
+window.hospitalData = [
+  {
+    id: 0,
+    district: "台北市",
+    name: "不萊梅特殊寵物專科醫院",
+    address: "台北市大同區民權西路227號",
+    phone: "02-25993907",
+    note: ""
+  },
+  {
+    id: 1,
+    district: "台北市",
+    name: "台大動物醫院",
+    address: "台北市大安區基隆路三段153號",
+    phone: "02-27396828",
+    note: ""
+  },
+  {
+    id: 2,
+    district: "台北市",
+    name: "伊甸動物醫院",
+    address: "台北市中山區北安路554巷33號1樓",
+    phone: "02-85097579",
+    note: ""
+  },
+  {
+    id: 3,
+    district: "台北市",
+    name: "亞馬森特寵專科醫院",
+    address: "台北市內湖區成功路四段311號",
+    phone: "02-87923248",
+    note: ""
+  },
+  {
+    id: 4,
+    district: "台北市",
+    name: "中研動物醫院",
+    address: "台北市南港區研究院路一段72號",
+    phone: "02-26512100",
+    note: ""
+  },
+  {
+    id: 5,
+    district: "台北市",
+    name: "良醫動物醫院",
+    address: "台北市松山區八德路四段188號",
+    phone: "02-27615091",
+    note: ""
+  },
+  {
+    id: 6,
+    district: "台北市",
+    name: "澄毅動物醫院",
+    address: "台北市大安區安和路2段171巷13號1樓",
+    phone: "02-2733-4341",
+    note: ""
+  },
+  {
+    id: 7,
+    district: "台北市",
+    name: "牧光特殊寵物專科醫院",
+    address: "臺北市大同區民族西路65號",
+    phone: "02-25926590",
+    note: "捷運圓山站1號出口步行五分鐘"
+  },
+  {
+    id: 8,
+    district: "台北市",
+    name: "沐沐鼠兔鳥爬專科動物醫院（預約制）",
+    address: "台北市中山區龍江路78號",
+    phone: "02 7713 7707",
+    note: ""
+  },
+  {
+    id: 9,
+    district: "台北市",
+    name: "萊特動物醫院",
+    address: "台北市大安區辛亥路一段50號",
+    phone: "02 2365 8628",
+    note: ""
+  },
+  {
+    id: 10,
+    district: "新北市",
+    name: "剛果非犬貓動物醫院",
+    address: "新北市新店區中興路三段259號",
+    phone: "02-86655702",
+    note: "已不看陸龜"
+  },
+  {
+    id: 11,
+    district: "新北市",
+    name: "展翔動物醫院",
+    address: "新北市板橋區南雅西路二段97號",
+    phone: "02-89650318",
+    note: ""
+  },
+  {
+    id: 12,
+    district: "新北市",
+    name: "馬達加斯加動物醫院",
+    address: "新北市板橋區文化路二段500號",
+    phone: "02-87595001",
+    note: ""
+  },
+  {
+    id: 13,
+    district: "新北市",
+    name: "台大文仁動物醫院",
+    address: "新北市板橋區篤行路三段18號",
+    phone: "02-26833122",
+    note: ""
+  },
+  {
+    id: 14,
+    district: "新北市",
+    name: "慈恩動物醫院",
+    address: "新北市永和區永利路142號",
+    phone: "02-32335195",
+    note: ""
+  },
+  {
+    id: 15,
+    district: "新北市",
+    name: "嘉德動物醫院",
+    address: "新北市汐止區福德一路206巷1號",
+    phone: "02-26934809",
+    note: ""
+  },
+  {
+    id: 16,
+    district: "新北市",
+    name: "獴獴加非犬貓專科醫院",
+    address: "新北市三重區重新路四段20號",
+    phone: "02-29792232",
+    note: ""
+  },
+  {
+    id: 17,
+    district: "新北市",
+    name: "金氏動物醫院",
+    address: "新北市新店區中興路一段208號",
+    phone: "02 8914 6999",
+    note: ""
+  },
+  {
+    id: 18,
+    district: "新北市",
+    name: "頂點動物醫院",
+    address: "新北市永和區仁愛路87號",
+    phone: "02 8921 1700",
+    note: ""
+  },
+  {
+    id: 19,
+    district: "新北市",
+    name: "綠野特殊寵物專科醫院",
+    address: "新北市中和區景平路335-1號",
+    phone: "02 2946 8818",
+    note: ""
+  },
+  {
+    id: 20,
+    district: "宜蘭縣",
+    name: "上寶貝動物醫院",
+    address: "宜蘭縣羅東鎮純精路二段137號",
+    phone: "03 951 7137",
+    note: ""
+  },
+  {
+    id: 21,
+    district: "宜蘭縣",
+    name: "東園動物醫院",
+    address: "宜蘭縣宜蘭市幸福路9號",
+    phone: "03-9327752",
+    note: ""
+  },
+  {
+    id: 22,
+    district: "宜蘭縣",
+    name: "噶瑪蘭動物醫院",
+    address: "宜蘭縣宜蘭市新興路119號",
+    phone: "03-9326119",
+    note: ""
+  },
+  {
+    id: 23,
+    district: "宜蘭市",
+    name: "",
+    address: "",
+    phone: "",
+    note: "目前沒有"
+  },
+  {
+    id: 24,
+    district: "基隆市",
+    name: "汎汶動物醫院",
+    address: "基隆市仁愛區仁四路17之1號(基隆廟口阿華炒麵附近)",
+    phone: "02-24260870",
+    note: ""
+  },
+  {
+    id: 25,
+    district: "桃園縣",
+    name: "",
+    address: "",
+    phone: "",
+    note: "目前沒有"
+  },
+  {
+    id: 26,
+    district: "桃園市",
+    name: "普羅動物醫院 ",
+    address: "桃園市桃園區泰成路15號",
+    phone: "03-3789900",
+    note: ""
+  },
+  {
+    id: 27,
+    district: "桃園市",
+    name: "晨曦動物醫院",
+    address: "桃園市桃園區國際路二段194號",
+    phone: "03-3600922",
+    note: ""
+  },
+  {
+    id: 28,
+    district: "桃園市",
+    name: "逗號動物醫院",
+    address: "桃園市桃園區中山路517號",
+    phone: "03-3322212",
+    note: ""
+  },
+  {
+    id: 29,
+    district: "桃園市",
+    name: "原野動物專科醫院",
+    address: "桃園市平鎮區廣德街12號1樓",
+    phone: "03-4942020",
+    note: ""
+  },
+  {
+    id: 30,
+    district: "桃園市",
+    name: "野森非犬貓動物醫院",
+    address: "桃園市中壢區民權路332號",
+    phone: "03-4910302",
+    note: ""
+  },
+  {
+    id: 31,
+    district: "新竹市",
+    name: "大福小幸動物警院",
+    address: "新竹市香山區經國路三段92巷6號",
+    phone: "03-5300175",
+    note: ""
+  },
+  {
+    id: 32,
+    district: "新竹市",
+    name: "新竺動物醫院",
+    address: "新竹市竹光路98號",
+    phone: "03-5429961",
+    note: ""
+  },
+  {
+    id: 33,
+    district: "新竹市",
+    name: "秘境野生動物專科醫院",
+    address: "新竹縣竹北市自強六街15號",
+    phone: "03-6685559",
+    note: ""
+  },
+  {
+    id: 34,
+    district: "新竹市",
+    name: "艾諾動物醫院",
+    address: "新竹市東區科學園路106號1樓",
+    phone: "03 577 5899",
+    note: ""
+  },
+  {
+    id: 35,
+    district: "新竹市",
+    name: "度度鳥特殊寵物專科醫院",
+    address: "新竹市東區西大路315巷7號1樓",
+    phone: "0965-109093",
+    note: ""
+  },
+  {
+    id: 36,
+    district: "新竹縣",
+    name: "懷恩動物醫院",
+    address: "新竹縣湖口鄉中山五街59號",
+    phone: "03-5990477",
+    note: ""
+  },
+  {
+    id: 37,
+    district: "新竹縣",
+    name: "秘境野生動物專科醫院",
+    address: "新竹縣竹北市自強六街15號",
+    phone: "03-668 5559",
+    note: ""
+  },
+  {
+    id: 38,
+    district: "苗栗縣",
+    name: "苗栗鴻成賽鴿醫院 備註：邱治璋 醫生",
+    address: "苗栗縣公館鄉石墻村8鄰177號",
+    phone: "03-7232908 0919-705290",
+    note: ""
+  },
+  {
+    id: 39,
+    district: "台中市",
+    name: "艾利動物醫院",
+    address: "台中市西屯區惠中路二段41號",
+    phone: "04-2258-9518",
+    note: ""
+  },
+  {
+    id: 40,
+    district: "台中市",
+    name: "侏儸紀野生動物醫院",
+    address: "台中市西區英才路625號",
+    phone: "04-23757808",
+    note: ""
+  },
+  {
+    id: 41,
+    district: "台中市",
+    name: "全國動物醫院",
+    address: "台中市西區五權八街100號",
+    phone: "0800-784666",
+    note: ""
+  },
+  {
+    id: 42,
+    district: "台中市",
+    name: "小島動物醫院",
+    address: "台中市西區三民西路83號",
+    phone: "04-23767158",
+    note: ""
+  },
+  {
+    id: 43,
+    district: "台中市",
+    name: "春天動物醫院",
+    address: "台中市北區進化北路294先",
+    phone: "04-77350838",
+    note: ""
+  },
+  {
+    id: 44,
+    district: "台中市",
+    name: "感恩動物醫院",
+    address: "台中市北區忠明路131號",
+    phone: "04-23202590",
+    note: ""
+  },
+  {
+    id: 45,
+    district: "台中市",
+    name: "中興大學附屬動物醫院野動科",
+    address: "台中市西區向上路一段21號",
+    phone: "04-22870180",
+    note: ""
+  },
+  {
+    id: 46,
+    district: "台中市",
+    name: "凡賽爾動物醫院",
+    address: "台中市西屯區重慶路8號",
+    phone: "04-23121880",
+    note: ""
+  },
+  {
+    id: 47,
+    district: "台中市",
+    name: "毛克利野生動物醫院",
+    address: "台中市北屯區文心路四段690號",
+    phone: "04-22386609",
+    note: ""
+  },
+  {
+    id: 48,
+    district: "台中市",
+    name: "中山動物醫院",
+    address: "台中市沙鹿區台灣大道七段1000號",
+    phone: "04-26622403",
+    note: ""
+  },
+  {
+    id: 49,
+    district: "台中市",
+    name: "亞東綜合動物醫院",
+    address: "台中市北屯區昌平路一段27號",
+    phone: "04-2233 6101",
+    note: ""
+  },
+  {
+    id: 50,
+    district: "台中市",
+    name: "羽森林動物醫院",
+    address: "台中市東區旱溪西路一段552號",
+    phone: "04-2213 2373",
+    note: ""
+  },
+  {
+    id: 51,
+    district: "台中市",
+    name: "達爾文動物醫院",
+    address: "台中市西區博館路157號",
+    phone: "04-2326-2759",
+    note: ""
+  },
+  {
+    id: 52,
+    district: "台中市",
+    name: "英國皇家動物醫院 ",
+    address: "台中市西屯區河南路二段429號B1之2 ",
+    phone: "04-27073887 ",
+    note: ""
+  },
+  {
+    id: 53,
+    district: "台中市",
+    name: "歐米咖動物醫院",
+    address: "台中市南區建國南路一段246號",
+    phone: "04-2260-2199",
+    note: ""
+  },
+  {
+    id: 54,
+    district: "彰化縣",
+    name: "龍貓動物醫院",
+    address: "彰化縣鹿港鎮彰鹿路七段639號",
+    phone: "04-7773488",
+    note: ""
+  },
+  {
+    id: 55,
+    district: "彰化縣",
+    name: "叢林特殊寵物專科醫院",
+    address: "彰化縣員林市條和街350號1F",
+    phone: 48333232,
+    note: ""
+  },
+  {
+    id: 56,
+    district: "彰化市",
+    name: "快樂寵物醫院",
+    address: "彰化縣彰化市中山路三段155號",
+    phone: "04-7384978",
+    note: ""
+  },
+  {
+    id: 57,
+    district: "嘉義市",
+    name: "中華動物醫院(嘉義分院)",
+    address: "嘉義市民生北路143號",
+    phone: "05-222-5688",
+    note: ""
+  },
+  {
+    id: 58,
+    district: "嘉義市",
+    name: "嘉樂動物醫院",
+    address: "嘉義市民族路67號",
+    phone: "05-2773122",
+    note: ""
+  },
+  {
+    id: 59,
+    district: "台南市",
+    name: "大灣動物醫院",
+    address: "台南市永康區大灣路610號",
+    phone: "06-2732119",
+    note: ""
+  },
+  {
+    id: 60,
+    district: "台南市",
+    name: "山豬動物醫院",
+    address: "台南市安平區平通路525號",
+    phone: "06-2991006",
+    note: ""
+  },
+  {
+    id: 61,
+    district: "台南市",
+    name: "廣慈動物醫院",
+    address: "台南市中西區府前路二段199號",
+    phone: "06-2288126",
+    note: ""
+  },
+  {
+    id: 62,
+    district: "台南市",
+    name: "立安動物醫院",
+    address: "台南市中西區永華路一段186號",
+    phone: "06-2286538",
+    note: ""
+  },
+  {
+    id: 63,
+    district: "台南市",
+    name: "中美獸醫院",
+    address: "台南市府連路314號",
+    phone: "06-2379-123",
+    note: ""
+  },
+  {
+    id: 64,
+    district: "台南市",
+    name: "崇愛動物醫院",
+    address: "台南市東區崇明路463號(近崇明國小)",
+    phone: "06-2604-733",
+    note: ""
+  },
+  {
+    id: 65,
+    district: "台南市",
+    name: "凱旋動物醫院",
+    address: "台南市北區公園路591-34號號",
+    phone: "06 251 4727",
+    note: "24小時急診"
+  },
+  {
+    id: 66,
+    district: "高雄市",
+    name: "中興農十六動物醫院",
+    address: "高雄市鼓山區大順一路935號",
+    phone: "07-5503532",
+    note: ""
+  },
+  {
+    id: 67,
+    district: "高雄市",
+    name: "亞幸動物醫院",
+    address: "高雄市苓雅區光華一路12之1號",
+    phone: "07-7265577",
+    note: ""
+  },
+  {
+    id: 68,
+    district: "高雄市",
+    name: "蓋亞野生動物專科醫院",
+    address: "高雄市三民區建工路611號",
+    phone: "07-3929353",
+    note: ""
+  },
+  {
+    id: 69,
+    district: "高雄市",
+    name: "藍天動物醫院",
+    address: "高雄市三民區博愛一路139號",
+    phone: "07-3229200",
+    note: ""
+  },
+  {
+    id: 70,
+    district: "高雄市",
+    name: "大毛小毛動物醫院",
+    address: "高雄市三民區文濱路62號",
+    phone: "07-7809102",
+    note: ""
+  },
+  {
+    id: 71,
+    district: "高雄市",
+    name: "聖弘綜合動物醫院",
+    address: "高雄市三民區鼎中路753之37號",
+    phone: "07-3103002",
+    note: ""
+  },
+  {
+    id: 72,
+    district: "高雄市",
+    name: "梅西動物醫療中心",
+    address: "高雄市左營區文府路498號",
+    phone: "07-3503840",
+    note: ""
+  },
+  {
+    id: 73,
+    district: "高雄市",
+    name: "肯亞動物專業醫院",
+    address: "高雄市鳳山區光復路一段26號",
+    phone: "07-7105150",
+    note: ""
+  },
+  {
+    id: 74,
+    district: "高雄市",
+    name: "回生動物醫院",
+    address: "高雄市新興區新田路39號",
+    phone: "07-2720-688",
+    note: ""
+  },
+  {
+    id: 75,
+    district: "高雄市",
+    name: "左營家畜醫院",
+    address: "高雄市左營大路437號",
+    phone: "07-5832343",
+    note: ""
+  },
+  {
+    id: 76,
+    district: "高雄市",
+    name: "星羽動物醫院",
+    address: "高雄市林園區東林西路487號",
+    phone: "07-6413808",
+    note: ""
+  },
+  {
+    id: 77,
+    district: "高雄市",
+    name: "博聯動物醫院",
+    address: "高雄市鼓山區中華一路350號",
+    phone: "07-522-9169",
+    note: ""
+  },
+  {
+    id: 78,
+    district: "高雄市",
+    name: "本丸特殊寵物與貓專科醫院",
+    address: "高雄市左鎮區永豐路105號",
+    phone: "07-721-1089",
+    note: ""
+  },
+  {
+    id: 79,
+    district: "高雄市",
+    name: "毛毛動物醫院",
+    address: "高雄市大社區三民路270號",
+    phone: "07-3535316",
+    note: ""
+  },
+  {
+    id: 80,
+    district: "屏東縣",
+    name: "屏安獸醫院",
+    address: "屏東縣屏東市中山路61-12號",
+    phone: "08-7336069",
+    note: ""
+  },
+  {
+    id: 81,
+    district: "屏東縣",
+    name: "肯亞特殊動物醫院",
+    address: "屏東縣屏東市業朝一路192號",
+    phone: "08-7656655",
+    note: ""
+  },
+  {
+    id: 82,
+    district: "屏東縣",
+    name: "大同獸醫院",
+    address: "屏東縣屏東市民族路222號",
+    phone: "08-7339215",
+    note: ""
+  },
+  {
+    id: 83,
+    district: "屏東縣",
+    name: "潮州國太動物醫院",
+    address: "屏東縣潮州鎮永昌路47巷3號",
+    phone: "08-780-2856 ",
+    note: ""
+  },
+  {
+    id: 84,
+    district: "花蓮市",
+    name: "中華動物醫院",
+    address: "花蓮市中華路325號之7",
+    phone: "03-8335123",
+    note: ""
+  },
+  {
+    id: 85,
+    district: "花蓮市",
+    name: "蕙康動物醫院",
+    address: "花蓮縣花蓮市信義街113-1號",
+    phone: "03-8352-122",
+    note: ""
+  }
+];
