7fc1083d019e1501fac6cd622c57792dbfb3b2f3
olivia bti <olivia.at.bit@gmail.com>
2024-12-22 14:28:45 +0800

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // 启用静态导出
+  basePath: "/tps",
   reactStrictMode: true, // 可选：启用严格模式
   // images: {
   //   unoptimized: true, // 可选：如果你不使用 Next.js 的图片优化功能，设置此项为 true
